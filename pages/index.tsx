6be8efb07a6a90736f01ad8a2eba59ff5b218b2d
howdyfrom2019 <tjd1emd@naver.com>
2022-10-21 18:13:53 +0900

    rewrite and ssr setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,53 @@
 import {useEffect, useState} from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "4e51c343f2941ca1820cdc001197944e";
-
-const Index = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    (async() => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-        )
-      ).json();
-      setMovies(results);
-    })();
-  }, []);
-
+const Index = ({ results }: any) => {
   return (
     <div>
-      <Seo title={"Home"} />
-      {! movies && <h4>Loading...</h4>}
-      {movies?.map((movie: any) => (
-        <div key={movie.id}>
-          <h4>{movie.original_title}</h4>
-        </div>
-      ))}
+      <div className={"container"}>
+        <Seo title={"Home"}/>
+        {results?.map((movie: any) => (
+          <div key={movie.id}>
+            <div className="movie" key={movie.id}>
+              <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={"poster"}/>
+              <h4>{movie.original_title}</h4>
+            </div>
+          </div>
+        ))}
+        <style jsx>{`
+          .container {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            padding: 20px;
+            gap: 20px;
+          }
+
+          .movie img {
+            max-width: 100%;
+            border-radius: 12px;
+            transition: transform 0.2s ease-in-out;
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+          }
+
+          .movie:hover img {
+            transform: scale(1.05) translateY(-10px);
+          }
+
+          .movie h4 {
+            font-size: 18px;
+            text-align: center;
+          }
+        `}</style>
+      </div>
     </div>
   );
 }
 
 export default Index;
+
+export const getServerSideProps = async() => {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: { results },
+  };
+}
