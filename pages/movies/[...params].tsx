d62fb919fa76fca04600b0e6b71e3d7890a0fdc3
howdyfrom2019 <tjd1emd@naver.com>
2022-10-22 17:53:44 +0900

    seo set.

--- pages/movies/[...params].tsx
+++ pages/movies/[...params].tsx
@@ -1,14 +1,15 @@
 import {useRouter} from "next/router";
 import {GetServerSidePropsContext, InferGetServerSidePropsType} from "next";
+import Seo from "../../components/Seo";
 
 type MovieDetailParams = [string, string] | [];
 
 const Detail = ({ params } : InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
-  console.log(params);
   const [title, id] = (router.query.params || []) as MovieDetailParams;
   return (
     <div>
+      <Seo title={title || ""} />
       <h4>{title || "Loading...."}</h4>
     </div>
   )
